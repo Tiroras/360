bad43330682857408a5046b13ee8c694b76a977e
Tiroras <shnyrov2000@gmail.com>
2021-06-13 23:48:10 +0400

    end

--- db.ts
+++ db.ts
@@ -1,9 +1,9 @@
 import {Sequelize} from "sequelize";
 
 
-const sequelize = new Sequelize("360", "root", "", {
+const sequelize = new Sequelize("s86206_db", "admin", "bugi2021", {
   dialect: "mysql",
-  host: "localhost",
+  host: "31.172.66.143",
   define: {
     timestamps: false
   }

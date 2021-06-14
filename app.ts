e5ba2f4231c12faa306296e34dcc0bf9bdc8ca1e
Tiroras <shnyrov2000@gmail.com>
2021-06-14 13:11:05 +0400

    debug

--- app.ts
+++ app.ts
@@ -13,16 +13,12 @@ app.use('/api/auth', authRouter);
 app.use('/api/users', usersRouter);
 app.use('/api/polls', pollsRouter);
 
-if(process.env.NODE_ENV === "production"){
-  try {
-    app.use("/", express.static(path.join(__dirname, 'client', 'build')));
-    app.get('*', (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
-    })
-  } catch (e) {
-    console.log(e)
-  }
-}
+
+app.use("/", express.static(path.join(__dirname, 'client', 'build')));
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+})
+
 
 const PORT = process.env.PORT || 5000;
 

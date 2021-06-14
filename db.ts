d0d80c7a57d0a08aff7a0404b01b1f4d5d9922fb
Tiroras <shnyrov2000@gmail.com>
2021-06-14 21:39:15 +0400

    Debug

--- db.ts
+++ db.ts
@@ -5,7 +5,6 @@ import {Sequelize} from "sequelize";
 const sequelize = new Sequelize("360", "root", "root", {
   dialect: "mysql",
   host: "localhost",
-  port: 5000,
   define: {
     timestamps: false
   }

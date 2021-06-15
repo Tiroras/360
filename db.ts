b70df6b5c710c6ba5c57bae9ce5d373d2823a950
Tiroras <shnyrov2000@gmail.com>
2021-06-15 17:50:38 +0400

    debug

--- db.ts
+++ db.ts
@@ -4,7 +4,7 @@ import {Sequelize} from "sequelize";
 
 const sequelize = new Sequelize("360", "root", "root", {
   dialect: "mysql",
-  host: "localhost",
+  host: "360appraisal-test.ru",
   define: {
     timestamps: false
   }
